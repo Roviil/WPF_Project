ff1f896006a0743e1a9562e3c3702e9ee8798ab8
qowjd <qowjdgns0106@naver.com>
2024-10-30 22:51:42 +0900

    초기 커밋.

---
+++ UiDesktopApp1/ViewModels/Pages/DashboardViewModel.cs
@@ -0,0 +1,35 @@
+﻿using UiDesktopApp1.interfaces;
+
+namespace UiDesktopApp1.ViewModels.Pages
+{
+    public partial class DashboardViewModel : ObservableObject
+    {
+        private readonly IDateTime _idateTime;
+
+        [ObservableProperty]
+        private string? text = string.Empty;
+
+        [ObservableProperty]
+        private int _counter = 0;
+
+        [ObservableProperty]
+        private String? currentTime = String.Empty;
+
+        public DashboardViewModel(IDateTime dateTime)
+        {
+            this._idateTime = dateTime;
+        }
+
+        [RelayCommand]
+        private void OnCounterIncrement()
+        {
+            this.Text = "Clicked";
+        }
+
+        [RelayCommand]
+        private void OnTextChanged()
+        {
+            this.CurrentTime = this._idateTime.GetCurrentTime().ToString();
+        }
+    }
+}
